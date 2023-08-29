faddb240313b08b26fbca752945ff28fdbcf8b88
lonebots <jishnusurajila@gmail.com>
2023-08-29 18:25:34 +0530

    chore: updated dates, added inverse relation from chirps to user

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,20 +1,29 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { useForm, Head } from '@inertiajs/vue3';
- 
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { useForm, Head } from "@inertiajs/vue3";
+import Chirp from "@/Components/Chirp.vue";
+
+defineProps(["chirps"]);
+
 const form = useForm({
-    message: '',
+    message: "",
 });
 </script>
- 
+
 <template>
     <Head title="Chirps" />
- 
+
     <AuthenticatedLayout>
         <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            <form @submit.prevent="form.post(route('chirps.store'), { onSuccess: () => form.reset() })">
+            <form
+                @submit.prevent="
+                    form.post(route('chirps.store'), {
+                        onSuccess: () => form.reset(),
+                    })
+                "
+            >
                 <textarea
                     v-model="form.message"
                     placeholder="What's on your mind?"
@@ -23,6 +32,11 @@ const form = useForm({
                 <InputError :message="form.errors.message" class="mt-2" />
                 <PrimaryButton class="mt-4">Chirp</PrimaryButton>
             </form>
+
+            <!-- display the list of created chirps -->
+            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <Chirp v-for="chirp in chirps" :key="chirp.id" :chirp="chirp" />
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
